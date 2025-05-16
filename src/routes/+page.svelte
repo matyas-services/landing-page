31ed41267c8e901687c56ebe38791fc2a7a489a0
matyii <kmatyas2004@gmail.com>
2025-05-16 23:45:57 +0200

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<div class="min-h-screen flex flex-col items-center justify-center bg-black">
+    <h1 class="text-6xl font-bold text-white mb-4 text-center">
+        matyas.services
+    </h1>
+    <p class="text-lg text-gray-400 text-center max-w-xl">
+        soon...
+    </p>
+</div>
