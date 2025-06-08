8e77414b93c9885ec03ab3cfc8b61d53e1477cf6
matyii <kmatyas2004@gmail.com>
2025-06-08 17:21:13 +0200

    test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <div class="min-h-screen flex flex-col items-center justify-center bg-black px-4">
     <h1 class="text-4xl sm:text-6xl font-bold text-white mb-4 text-center break-words">
-        matyas.services dev page
+        matyas.services stripe test
     </h1>
     <p class="text-base sm:text-lg text-gray-400 text-center max-w-md sm:max-w-xl">
         soon...
